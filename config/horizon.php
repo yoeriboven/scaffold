cb6244fcc2e0ff32303787b1c347e44b6da08659
Yoeri Boven <yoeri@yoeri.me>
2026-06-04 18:47:38 +0200

    Update horizon.php

--- config/horizon.php
+++ config/horizon.php
@@ -209,7 +209,7 @@ return [
             'maxJobs' => 0,
             'memory' => 128,
             'tries' => 1,
-            'timeout' => 60,
+            'timeout' => 95,
             'nice' => 0,
         ],
     ],
